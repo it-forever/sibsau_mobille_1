8fe2ac216e4afe952f850bcbaec4720f5193c6ba
Alex Tkachenko <tan@it-forever.ru>
2021-01-22 17:12:03 +0300

    add tip calculator

---
+++ TipCalculator/app/src/main/java/ru/itforever/tipcalculator/Calc.java
@@ -0,0 +1,17 @@
+package ru.itforever.tipcalculator;
+
+public class Calc {
+    private double billAmount; // Сумма счёта
+    private double percent; // Процент чаевых
+    public double calculateTip(double billAmount, double percent) { return billAmount*percent; }
+    public double calculateTotal(double billAmount, double percent) { return billAmount*(1+percent);}
+    // Getter and setter:
+    public double getBillAmount() { return billAmount; }
+    public void setBillAmount(double billAmount) { this.billAmount = billAmount; }
+
+    public double getPercent() { return percent; }
+    public void setPercent(double percent) { this.percent = percent; }
+
+
+
+}
